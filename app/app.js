7a8a6053db48c0bbf4e89f8eb5b34165ca25bfb7
keiichiwatanuki <edu.d.rai@gmail.com>
2016-04-04 10:49:09 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,22 +1,19 @@
 var app = angular.module('app',[]);
-
 var notas = function(myScope){
 	myScope.notitas =
-        [{id:1,title:"fuuuuck",description:"more fuuuck",done:false},
-            {id:2,title:"loool",description:"lalala",done:true}];
-        myScope.remove = function(algo) {
-            myScope.notitas.splice(algo.id - 1, 1);
-            for (var i = algo.id - 1; i < myScope.notitas.length; i++) {
-                if(myScope.notitas[i].id != 1)
-                myScope.notitas[i].id = myScope.notitas[i].id-1;
-            }
-        }
-        myScope.add = function() {
-            this.nota.id = myScope.notitas.length+1;
-            this.nota.done = false;
-            myScope.notitas.push(angular.copy(this.nota));}
-};
-
+		[{id:1,title:"Hacer mock",description:"mock working",done:true},
+		{id:2,title:"aprender a usar router ui",description:"lo que dice la descripcion",done:false}];
+	myScope.remove = function(algo) {
+	myScope.notitas.splice(algo.id - 1, 1);
+	for (var i = algo.id - 1; i < myScope.notitas.length; i++) {
+		if(myScope.notitas[i].id != 1)
+			myScope.notitas[i].id = myScope.notitas[i].id-1;
+	}
+			}
+	myScope.add = function() {
+		this.nota.id = myScope.notitas.length+1;
+		this.nota.done = false;
+		myScope.notitas.push(angular.copy(this.nota));}
+	};
 notas.$inject = ['$scope'];
-
 app.controller('notas',notas);
